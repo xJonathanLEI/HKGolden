fdffd1bf5a4387df72db9bf3eea63b8d11f54d1a
Jonathan LEI <xJonathan@outlook.com>
2016-08-18 16:32:28 +0800

    Added post-related types

---
+++ HKGoldenAPI/Types/PostRelated.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HKGoldenAPI.Types
+{
+    public class Post
+    {
+        public string messageID { get; set; }
+        public string postTitle { get; set; }
+        public List<PostPage> pages { get; set; }
+    }
+
+    public class PostPage
+    {
+        public bool pageLoaded { get; set; }
+        public List<PostEntry> postEntries { get; set; }
+    }
+
+    public class PostEntry
+    {
+        public PostAuthor entryAuthor { get; set; }
+        public string entryContent { get; set; }
+        public DateTime postTime { get; set; }
+    }
+
+    public class PostAuthor
+    {
+        public string userID { get; set; }
+        public string nickname { get; set; }
+    }
+}
